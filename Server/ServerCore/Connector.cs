c31cc828593053b545b4fb6ac3710f13f6bd388d
JUNG HOO CHOI <chl9430@gmail.com>
2025-03-19 14:43:07 +0900

    I have added a Server Core project file to the server solution that plays an important role in server operations.

---
+++ Server/ServerCore/Connector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    public class Connector
+    {
+        Func<Session> _sessionFactory;
+
+        void RegisterConnect(SocketAsyncEventArgs args)
+        {
+            Socket socket = args.UserToken as Socket;
+            if (socket == null)
+                return;
+
+            bool pending = socket.ConnectAsync(args);
+            if (pending == false)
+                OnConnectedCompleted(null, args);
+        }
+
+        void OnConnectedCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                Session session = _sessionFactory.Invoke();
+                session.Start(args.ConnectSocket);
+                session.OnConnected(args.RemoteEndPoint);
+            }
+            else
+            {
+                Console.WriteLine($"OnConnectedCompleted Fail : {args.SocketError}");
+            }
+        }
+    }
+}
