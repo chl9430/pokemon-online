3949aa76890f751f9a5d971cd4a38aab598cb538
JUNG HOO CHOI <chl9430@gmail.com>
2025-03-21 14:39:57 +0900

    I created a JobSerializer class that manages all Job processing orders.

---
+++ Server/Server/Game/Job/Job.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public interface IJob
+    {
+        void Execute();
+    }
+
+    public class Job : IJob
+    {
+        Action _action;
+
+        public Job(Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute()
+        {
+            _action.Invoke();
+        }
+    }
+
+    public class Job<T1> : IJob
+    {
+        Action<T1> _action;
+        T1 _t1;
+
+        public Job(Action<T1> action, T1 t1)
+        {
+            _action = action;
+            _t1 = t1;
+        }
+
+        public void Execute()
+        {
+            _action.Invoke(_t1);
+        }
+    }
+}
---
+++ Server/Server/Game/Job/JobSerializer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class JobSerializer
+    {
+        JobTimer _timer = new JobTimer();
+        Queue<IJob> _jobQueue = new Queue<IJob>();
+        object _lock = new object();
+        bool _flush = false;
+
+        public void PushAfter(int tickAfter, Action action) { PushAfter(tickAfter, new Job(action)); }
+        public void PushAfter<T1>(int tickAfter, Action<T1> action, T1 t1) { PushAfter(tickAfter, new Job<T1>(action, t1)); }
+
+        public void PushAfter(int tickAfter, IJob job)
+        {
+            _timer.Push(job, tickAfter);
+        }
+
+        public void Push(Action action) { Push(new Job(action)); }
+        public void Push<T1>(Action<T1> action, T1 t1) { Push(new Job<T1>(action, t1)); }
+
+        public void Push(IJob job)
+        {
+            lock (_lock)
+            {
+                _jobQueue.Enqueue(job);
+            }
+        }
+
+        public void Flush()
+        {
+            _timer.Flush();
+
+            while (true)
+            {
+                IJob job = Pop();
+                if (job == null)
+                    return;
+
+                job.Execute();
+            }
+        }
+
+        IJob Pop()
+        {
+            lock (_lock)
+            {
+                if (_jobQueue.Count == 0)
+                {
+                    _flush = false;
+                    return null;
+                }
+                return _jobQueue.Dequeue();
+            }
+        }
+    }
+}
