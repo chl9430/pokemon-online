be21fe325d33ae18d3e3440fcf07f33601679704
JUNG HOO CHOI <chl9430@gmail.com>
2025-04-02 19:54:16 +0900

    I created an ObjectManager class that manages all GameObject classes in the server.

---
+++ Server/Server/Game/Object/GameObject.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class GameObject
+    {
+        public GameObjectType ObjectType { get; protected set; } = GameObjectType.None;
+
+        public int Id
+        {
+            get { return Info.ObjectId; }
+            set { Info.ObjectId = value; }
+        }
+
+        public GameRoom Room { get; set; }
+        public ObjectInfo Info { get; set; } = new ObjectInfo();
+
+        public GameObject()
+        {
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
