451ba2f7eb95d9ef4335491caef30fd71b088179
JUNG HOO CHOI <chl9430@gmail.com>
2025-04-03 03:00:21 +0900

    I automatically generated the packet-related code using the PacketGenerator project and protocol buffer.

--- Common/protoc-30.1-win64/bin/GenProto.bat
+++ Common/protoc-30.1-win64/bin/GenProto.bat
@@ -1,8 +1,8 @@
 protoc.exe -I=./ --csharp_out=./ ./Protocol.proto 
 IF ERRORLEVEL 1 PAUSE
 
-REM START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
+START ../../../Server/PacketGenerator/bin/Debug/net8.0/PacketGenerator.exe ./Protocol.proto
 REM XCOPY /Y Protocol.cs "../../../Client/Assets/Scripts/Packet"
-REM XCOPY /Y Protocol.cs "../../../Server/Server/Packet"
+XCOPY /Y Protocol.cs "../../../Server/Server/Packet"
 REM XCOPY /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
-REM XCOPY /Y ServerPacketManager.cs "../../../Server/Server/Packet"
+XCOPY /Y ServerPacketManager.cs "../../../Server/Server/Packet"
