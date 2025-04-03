50cd172a45b5e734d555c918a96460cb0fe33c08
JUNG HOO CHOI <chl9430@gmail.com>
2025-04-03 18:59:00 +0900

    I created a GameRoom class that allows players to communicate with each other.

---
+++ Server/Server/Game/Room/GameRoom.cs
@@ -0,0 +1,86 @@
+﻿using Google.Protobuf.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Server
+{
+    public class GameRoom : JobSerializer
+    {
+        public int RoomId { get; set; }
+
+        Dictionary<int, Player> _players = new Dictionary<int, Player>();
+
+        public void Init(int mapId)
+        {
+
+        }
+
+        public void Update()
+        {
+            Flush();
+        }
+
+        public void EnterGame(GameObject gameObject)
+        {
+            if (gameObject == null)
+                return;
+
+            GameObjectType type = ObjectManager.GetObjectTypeById(gameObject.Id);
+
+            if (type == GameObjectType.Player)
+            {
+                Player player = gameObject as Player;
+                _players.Add(gameObject.Id, player);
+                player.Room = this;
+
+                // 본인한테 정보 전송
+                S_EnterGame enterPacket = new S_EnterGame();
+                enterPacket.Player = player.Info;
+                player.Session.Send(enterPacket);
+            }
+
+            // 타인한테 정보 전송
+            S_Spawn spawnPacket = new S_Spawn();
+            spawnPacket.Objects.Add(gameObject.Info);
+
+            foreach(Player p in _players.Values)
+            {
+                if (p.Id != gameObject.Id)
+                    p.Session.Send(spawnPacket);
+            }
+        }
+
+        public void LeaveGame(int objectId)
+        {
+            GameObjectType type = ObjectManager.GetObjectTypeById(objectId);
+
+            if (type == GameObjectType.Player)
+            {
+                Player player = null;
+                if (_players.Remove(objectId, out player) == false)
+                    return;
+
+                player.Room = null;
+
+                // 본인한테 정보 전송
+                S_LeaveGame leavePacket = new S_LeaveGame();
+                player.Session.Send(leavePacket);
+            }
+
+            // 타인한테 정보 전송
+            S_Despawn despawnPacket = new S_Despawn();
+            despawnPacket.ObjectIds.Add(objectId);
+            
+            foreach (Player p in _players.Values)
+            {
+                if (p.Id != objectId)
+                    p.Session.Send(despawnPacket);
+            }
+        }
+    }
+}
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Program
+    {
+        static Listener _listener = new Listener();
+        static List<System.Timers.Timer> _timers = new List<System.Timers.Timer>();
+
+        static void TickRoom(GameRoom room, int tick = 100)
+        {
+            var timer = new System.Timers.Timer();
+            timer.Interval = tick;
+            timer.Elapsed += ((s, e) => { room.Update(); });
+            timer.AutoReset = true;
+            timer.Enabled = true;
+
+            _timers.Add(timer);
+        }
+
+        static void Main(string[] args)
+        {
+            GameRoom room = RoomManager.Instance.Add(1);
+            TickRoom(room, 50);
+
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            _listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
