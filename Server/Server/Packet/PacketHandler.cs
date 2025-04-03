4cadabca4d7bbada6113f50f0a44899bb29506d1
JUNG HOO CHOI <chl9430@gmail.com>
2025-04-03 18:08:27 +0900

    I created a PacketHandler class that allows me to define what to process when the Server receives a packet.

---
+++ Server/Server/Packet/PacketHandler.cs
@@ -0,0 +1,20 @@
+﻿using Google.Protobuf;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PacketHandler
+{
+    public static void C_MoveHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+
+    public static void C_SkillHandler(PacketSession session, IMessage packet)
+    {
+
+    }
+}
